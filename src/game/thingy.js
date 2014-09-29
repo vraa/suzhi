f18f5a0158702ed0f6a12a7208a29579cbf2538f
Veera <veera.k.sundar@gmail.com>
2014-09-29 20:07:25 +0530

    Animate baddies.

--- src/game/thingy.js
+++ src/game/thingy.js
@@ -4,40 +4,40 @@ define(['util', 'colors'], function(util,colors){
 
 	var config = {
 		smiley : {
+			role : 'goody',
 			coords : coords.smiley,
 			value : 5,
-			tX : 15,
-			tY : 10,
 			color: colors.goody
 		}, laughy : {
+			role : 'goody',
 			coords : coords.laughy,
 			value : 50,
-			tX : 13,
-			tY : 12,
 			color: colors.goody
 		}, holy : {
+			role : 'goody',
 			coords : coords.holy,
 			value : 200,
-			tX : 10,
-			tY : 12,
 			color: colors.goody
 		}, geary : {
+			role : 'baddie',
 			coords : coords.geary['1'],
+			coordsOdd : coords.geary['1'],
+			coordsEven : coords.geary['2'],
 			value : -5,
-			tX : 15,
-			tY : 10,
 			color: colors.baddie
 		}, sunny : {
+			role : 'baddie',
 			coords : coords.sunny['1'],
+			coordsOdd : coords.sunny['1'],
+			coordsEven : coords.sunny['2'],
 			value : -50,
-			tX : 13,
-			tY : 12,
 			color: colors.baddie
 		}, rady : {
+			role : 'baddie',
 			coords : coords.rady['1'],
+			coordsOdd : coords.rady['1'],
+			coordsEven : coords.rady['2'],
 			value : -100,
-			tX : 10,
-			tY : 12,
 			color: colors.baddie
 		}
 	}, margin = 30;
@@ -46,17 +46,21 @@ define(['util', 'colors'], function(util,colors){
 		this.type = options.type;
 		this.value = config[this.type].value;
 		this.coords = config[this.type].coords;
+		this.coordsOdd = config[this.type].coordsOdd;
+		this.coordsEven = config[this.type].coordsEven;
 		this.color = config[this.type].color;
+		this.role = config[this.type].role;
 		this.x = options.position.x;
 		this.y = options.position.y;
-		this.tX = config[this.type].tX;
-		this.tY = config[this.type].tY;
+		this.width = this.coords[2];
+		this.height = this.coords[3];
 		this.xVelocity = 1;
 		this.yVelocity = 1;
 		this.cH = options.canvas.height;
 		this.cW = options.canvas.width;
 		this.lifeTime = 500;
 		this.sprite = options.sprite;
+		this.animFrames = 0;
 	}
 
 	Thingy.prototype = {
@@ -122,24 +126,26 @@ define(['util', 'colors'], function(util,colors){
 				this.isCaptured = true;
 				this.lifeTime = 0;
 			}
+			if(this.role === 'baddie'){
+				if(this.animFrames >= 10){
+					this.animFrames = 0;
+					this.coords =  frames % 2 === 0 ? this.coordsEven : this.coordsOdd;	
+				}else{
+					this.animFrames++;
+				}
+				
+			}
 		},
 		draw : function(ctx){
-			var fontSize;
+			var tx,ty,sign;
 			ctx.save();
-
 			util.drawSprite(ctx, this.sprite, this.coords, this.x, this.y);
-			/*
-			if(! (this.isDying || this.isCollided)){
-				ctx.font = '7pt Arial';
-				var sign = this.value < 0 ? '' : '+';
-				ctx.fillText(sign + this.value, this.x + this.tX, this.y+this.tY);
-			}*/
-			/*ctx.beginPath();
-			ctx.fillStyle = 'rgba(255,255,255,.5)';
-			ctx.arc(this.x + 18,this.y - 15,20, 0, Math.PI * 2, true);
-			ctx.fill();
-			ctx.closePath();
-			*/
+			ctx.fillStyle = this.color;
+			ctx.font = '7pt Courier';
+			sign = this.value < 0 ? '' : '+';
+			tx = this.x + (this.width / 2) - 10;
+			ty = this.y + (this.height + 10);
+			ctx.fillText(sign + this.value,tx,ty);
 			ctx.restore();
 
 
